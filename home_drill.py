56c10433df0344e3e80b80dabe19c70cbf04cc9e
callmefarad <65671589+callmefarad@users.noreply.github.com>
2020-10-07 13:40:32 +0100

    Problem solved - Ubani Friday

--- home_drill.py
+++ home_drill.py
@@ -4,4 +4,20 @@
 # displaying the balance he came back with.
 
 # Enter your solution here:
+# enter the capital
+capital = float(input("Enter the amount taken to the super market: "))
+
+# enter the amount of the different product
+soap = float(input("Enter the amount of soap bought: "))
+milk = float(input("Enter the amount of milk bought: "))
+juice = float(input("Enter the amount of juice bought: "))
+
+# sum the product bought
+total_ammount_product = soap + milk + juice
+
+# display the balance
+balance = capital - total_ammount_product
+
+print(balance)
+
 
